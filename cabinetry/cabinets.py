9244fa042462bff79607f99026200ec5a79af5f3
FickleHobbyist <alexnmay@gmail.com>
2022-10-31 19:20:54 -0700

    Removed file that was accidentally added

--- cabinetry/cabinets.py
+++
@@ -1,4 +0,0 @@
-"""Module containing high level cabinet classes for use in constructing larger collections of cabinets (i.e. a kitchen)"""
-from cabinetry.components import ComponentContainer, RectangularComponent
-from cabinetry.materials import Material
-
