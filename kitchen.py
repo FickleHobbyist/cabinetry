75c42c313f9c8de5ed1f7dad79db71866b1dec03
FickleHobbyist <alexnmay@gmail.com>
2022-11-10 16:45:41 -0800

    Added uppers

--- kitchen.py
+++ kitchen.py
@@ -4,13 +4,13 @@ from cabinetry.components import ComponentContainer, ComponentGrid, GhostCompone
 from cabinetry.components.uppers import UpperCabinet
 from cabinetry.components.lowers import LowerCabinet, Pantry
 from cabinetry.components.shelves import StandardShelf
+from cabinetry.components.doors import ShakerDoor
 from cabinetry.components.factory import _door_factory, _drawer_factory, get_faceframe_factory, get_shelf_factory
 from cabinetry.components.decorators import faceframe_with_shelves
 import numpy as np
 
 
 TOP_DRAWER_HEIGHT = 5
-CABINET_SPACING = 0.25
 
 
 def _N_Drawer_eq_args(num_drawers: int):
@@ -27,7 +27,7 @@ def _N_Drawer_offset_args(num_drawers: int):
     }
 
 
-def create_grid_with_spacing(cabs: list, spacing: float = CABINET_SPACING, **kwargs):
+def assemble_in_grid(cabs: list, spacing: float = 0, **kwargs):
     widths = [cab.width for cab in cabs]
     nCab = len(widths)
     name = kwargs.pop('name', 'global-frame')
@@ -51,13 +51,13 @@ def create_grid_with_spacing(cabs: list, spacing: float = CABINET_SPACING, **kwa
     return grid
 
 
-def main():
+def construct_kitchen():
     base_frame = ComponentContainer()
-    north_wall_lowers: list[LowerCabinet] = []
-    north_wall_lowers.append(
+    south_wall_lowers: list[LowerCabinet] = []
+    south_wall_lowers.append(
         Pantry(
             width=36,
-            height=90,
+            height=93,
             frame_args={
                 'row_dist': [90/3, 1],
                 'row_type': ['fixed', 'weighted'],
@@ -66,35 +66,35 @@ def main():
             }
         )
     )
-    north_wall_lowers.append(
+    south_wall_lowers.append(
         Pantry(
             width=15,
-            height=90,
+            height=93,
             frame_args={
-                'row_dist': [90/3, 1],
+                'row_dist': [93/3, 1],
                 'row_type': ['fixed', 'weighted'],
                 'col_dist': [1],
                 'col_type': ['weighted'],
             }
         )
     )
-    north_wall_lowers.append(
+    south_wall_lowers.append(
         LowerCabinet(
             width=21,
             frame_factory=get_faceframe_factory('N-Drawer'),
             frame_args=_N_Drawer_offset_args(3),
         )
     )
-    north_wall_lowers.append(
+    south_wall_lowers.append(
         LowerCabinet(
             width=18,
             frame_factory=get_faceframe_factory('N-Drawer'),
             frame_args=_N_Drawer_offset_args(3),
         )
     )
-    north_wall_lowers = create_grid_with_spacing(
-        cabs=north_wall_lowers,
-        spacing=CABINET_SPACING,
+    south_wall_lowers = assemble_in_grid(
+        cabs=south_wall_lowers,
+        spacing=0,
         name='north_wall_lowers',
         parent=base_frame,
         orientation=Orientation(
@@ -148,9 +148,9 @@ def main():
         )
     )
 
-    west_wall_lowers = create_grid_with_spacing(
+    west_wall_lowers = assemble_in_grid(
         cabs=west_wall_lowers,
-        spacing=CABINET_SPACING,
+        spacing=0,
         name='west_wall_lowers',
         parent=base_frame,
         orientation=Orientation(
@@ -165,9 +165,9 @@ def main():
         )
     )
 
-    east_wall_lowers: list[LowerCabinet] = []
+    north_wall_lowers: list[LowerCabinet] = []
     # Simulated Lemans corner cabinet
-    east_wall_lowers.append(
+    north_wall_lowers.append(
         LowerCabinet(
             width=45,
             frame_factory=get_faceframe_factory('N-Door-Horiz'),
@@ -192,10 +192,10 @@ def main():
     )
     # Add drawer to bottom cell of microwave_cab. Top remains open for microwave
     _drawer_factory(microwave_cab.face.cells[1])
-    east_wall_lowers.append(microwave_cab)
+    north_wall_lowers.append(microwave_cab)
 
     # Stand-in for corner cabinet on peninsula
-    east_wall_lowers.append(
+    north_wall_lowers.append(
         LowerCabinet(
             width=18,
             frame_factory=get_faceframe_factory('N-Door-Horiz'),
@@ -206,10 +206,10 @@ def main():
         )
     )
 
-    east_wall_lowers = create_grid_with_spacing(
-        cabs=east_wall_lowers,
-        spacing=CABINET_SPACING,
-        name='east_wall_lowers',
+    north_wall_lowers = assemble_in_grid(
+        cabs=north_wall_lowers,
+        spacing=0,
+        name='north_wall_lowers',
         parent=base_frame,
         orientation=Orientation(
             rx=0,
@@ -243,7 +243,7 @@ def main():
                     'dist_type': ['weighted', 'weighted'],
                 },
                 position=Position(
-                    x=(27 + CABINET_SPACING)*i,
+                    x=(27 + 0)*i,
                     y=0,
                     z=0,
                 )
@@ -262,7 +262,7 @@ def main():
             'col_type': ['weighted'],
         },
         position=Position(
-            x=(27 + CABINET_SPACING)*3+18,
+            x=(27 + 0)*3+18,
             y=0,
             z=0,
         ),
@@ -297,7 +297,7 @@ def main():
                 'dist_type': [*['fixed']*3, 'weighted'],
             },
             position=Position(
-                x=(27 + CABINET_SPACING)*3+18,
+                x=(27 + 0)*3+18,
                 y=48+1/8,
                 z=0,
             ),
@@ -320,7 +320,7 @@ def main():
             },
             position=Position(
                 x=island.children[-1].position.x -
-                (island.children[-1].width + CABINET_SPACING),
+                (island.children[-1].width + 0),
                 y=48+1/8,
                 z=0,
             ),
@@ -337,7 +337,7 @@ def main():
         width=36,
         position=Position(
             x=island.children[-1].position.x -
-            (island.children[-1].width + CABINET_SPACING),
+            (island.children[-1].width + 0),
             y=48+1/8,
             z=0,
         ),
@@ -371,8 +371,148 @@ def main():
     # Add doors to bottom cell of sink_cab. Top remains open for sink
     _door_factory(under_sink_grid.cells)
 
+    south_wall_uppers: list[UpperCabinet] = []
+    upper_widths = [30, 12]
+    num_doors = [2, 1]
+    nShelves = 3
+    for width, ndoors in zip(upper_widths, num_doors):
+        cab = UpperCabinet(
+            width=width,
+            frame_factory=get_faceframe_factory('N-Door-Horiz'),
+            frame_args={
+                'door_dist': [1]*ndoors,
+                'dist_type': ['weighted']*ndoors
+            },
+            # depth and height relative to front of lowers at the floor level
+            position=Position(
+                x=0,
+                y=Config.LOWERS_DEPTH-Config.UPPERS_DEPTH,
+                z=Config.COUNTER_HEIGHT+Config.COUNTER_TO_UPPERS_GAP,
+            )
+        )
+        south_wall_uppers.append(cab)
+
+    # Corner cabinet to be simulated by 24" wide and 12" wide
+    corner_cab_left = UpperCabinet(
+        width=24,
+        frame_factory=get_faceframe_factory('MxN-Empty'),
+        frame_args={
+            'row_dist': [1],
+            'row_type': ['weighted'],
+            'col_dist': [9, 1],
+            'col_type': ['fixed', 'weighted'],
+        },
+        # depth and height relative to front of lowers at the floor level
+        position=Position(
+            x=0,
+            y=Config.LOWERS_DEPTH-Config.UPPERS_DEPTH,
+            z=Config.COUNTER_HEIGHT+Config.COUNTER_TO_UPPERS_GAP,
+        )
+    )
+    # Add door to left side only
+    cell = corner_cab_left.face.cells[0, 0]
+    cell.add_child(
+        ShakerDoor(
+            hinge_side='left',
+            hinge_stile_factor='double',
+            opening_width=cell.width,
+            opening_height=cell.height,
+        )
+    )
+
+    # Adding 12" cabinet and rotating to make positioning it easier
+    # The door and case details aren't exactly right here but close
+    # enough for a rough estimate.
+    # TODO: Make a corner cabinet class to better represent this geometry
+    corner_cab_right = UpperCabinet(
+        parent=corner_cab_left,
+        width=12,
+        frame_factory=get_faceframe_factory('N-Door-Horiz'),
+        frame_args={
+            'door_dist': [1],
+            'dist_type': ['weighted'],
+        },
+        position=Position(
+            x=corner_cab_left.width-Config.UPPERS_DEPTH,
+            y=0,
+            z=0,
+        ),
+        orientation=Orientation(
+            rx=0,
+            ry=0,
+            rz=-90
+        )
+    )
+
+    south_wall_uppers.append(corner_cab_left)
+
+    for cab in south_wall_uppers:
+        shelf_grid = ComponentGrid(
+            parent=cab.case,
+            width=cab.case.box_width_inside,
+            height=cab.case.box_height_inside,
+            position=cab.case.box_inside_origin,
+            row_dist=np.array([1]*(nShelves+1)),
+            row_type=['weighted']*(nShelves+1),
+            col_dist=[1],
+            col_type=['weighted'],
+        )
+        get_shelf_factory('banded')(grid=shelf_grid, case=cab.case)
+
+    south_wall_uppers = assemble_in_grid(
+        cabs=south_wall_uppers,
+        spacing=0,
+        name='south_wall_uppers',
+        parent=base_frame,
+        orientation=Orientation(
+            rx=0,
+            ry=0,
+            rz=90,
+        ),
+        # position at origin of first lower cabinet
+        position=south_wall_lowers.cells[0, 2].get_global_pose()[0],
+    )
+
+    # pos, ori = corner_cab_right.get_global_pose()
+    west_wall_uppers: list[UpperCabinet] = []
+    west_wall_uppers.append(
+        UpperCabinet(
+            width=12,
+            frame_factory=get_faceframe_factory('N-Door-Horiz'),
+            frame_args={
+                'door_dist': [1],
+                'dist_type': ['weighted'],
+            },
+        )
+    )
+    west_wall_uppers.append(
+        UpperCabinet(
+            width=36,
+            frame_factory=get_faceframe_factory('N-Door-Horiz'),
+            frame_args={
+                'door_dist': [1]*2,
+                'dist_type': ['weighted']*2,
+            },
+        )
+    )
+
+    west_wall_uppers = assemble_in_grid(
+        cabs=west_wall_uppers,
+        spacing=0,
+        name='west_wall_uppers',
+        parent=base_frame,
+        position=(
+            corner_cab_right.get_global_pose()[0]
+            + (corner_cab_right.width+0, 0, 0)),
+        orientation=Orientation(
+            rx=0,
+            ry=0,
+            rz=0
+        )
+    )
+
     base_frame.render()
 
 
 if __name__ == "__main__":
-    main()
+    construct_kitchen()
