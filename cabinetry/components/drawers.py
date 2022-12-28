be7a9381f9eeb200b051986612a122ac741a4511
FickleHobbyist <alexnmay@gmail.com>
2022-12-27 23:10:00 -0800

    Added SimpleInsetDrawer and Qtr3DrawerBox (constructed with the Quarter-Quarter-Quarter system)

--- cabinetry/components/drawers.py
+++ cabinetry/components/drawers.py
@@ -2,6 +2,168 @@ from ..config import Config
 from ..base import Position, Orientation
 from ..materials import Material
 from . import ComponentContainer, RectangularComponent, ShakerFramedPanel
+from typing import Union
+
+class Qtr3DrawerBox(ComponentContainer):
+    def __init__(self,
+                 width: float,
+                 height: float,
+                 length: float,
+                 box_material: Material,
+                 bottom_material: Material,
+                 **kwargs):
+        self.width = width
+        self.height = height
+        self.length = length
+        self.box_material = box_material
+        self.bottom_material = bottom_material
+        # TODO: Assert bottom material thickness is ~ half box material thickness (within some tolerance?)
+        super().__init__(**kwargs)
+
+        L_Side = self.make_side()
+        self.add_child(L_Side)
+        R_Side = self.make_side()
+        R_Side.position.x = self.width
+        self.add_child(R_Side)
+
+        front_end = self.make_end()
+        self.add_child(front_end)
+        back_end = self.make_end()
+        back_end.position.y = self.length - self.box_material.thickness
+        self.add_child(back_end)
+
+        self.add_child(self.make_bottom())
+
+    def make_side(self) -> RectangularComponent:
+        side = RectangularComponent(
+            name='Qtr3DrawerBox Side',
+            width=self.length,
+            height=self.height,
+            material=self.box_material,
+            color=Config.DRAWER_BOX_COLOR,
+            position=Position(
+                x=self.box_material.thickness,
+                y=0,
+                z=0,
+            ),
+            orientation=Orientation(
+                rx=0,
+                ry=0,
+                rz=90,
+            ),
+        )
+        return side
+
+    def make_end(self) -> RectangularComponent:
+        end = RectangularComponent(
+            name='Qtr3DrawerBox End',
+            width=self.width - self.box_material.thickness,
+            height=self.height,
+            material=self.box_material,
+            color=Config.DRAWER_BOX_COLOR,
+            position=Position(
+                x=0.5*self.box_material.thickness,
+                y=0,
+                z=0,
+            ),
+            orientation=Orientation(
+                rx=0,
+                ry=0,
+                rz=0,
+            ),
+        )
+        return end
+
+    def make_bottom(self) -> RectangularComponent:
+        bottom = RectangularComponent(
+            name='Qtr3DrawerBox Bottom',
+            width=self.width - self.box_material.thickness,
+            height=self.length - self.box_material.thickness,
+            material=self.bottom_material,
+            color=Config.DRAWER_BOX_COLOR,
+            position=Position(
+                x=0.5*self.box_material.thickness,
+                y=0.5*self.box_material.thickness,
+                z=self.bottom_material.thickness + 0.5*self.box_material.thickness,
+            ),
+            orientation=Orientation(
+                rx=-90,
+                ry=0,
+                rz=0,
+            ),
+        )
+        return bottom
+
+
+class SimpleInsetDrawer(ComponentContainer):
+    def __init__(self,
+                 opening_width: float,
+                 opening_height: float,
+                 length: float,
+                 face_material: Material,
+                 box_material: Material,
+                 bottom_material: Material,
+                 reveal: Union[float, tuple[float]],
+                 drawer_slide_thickness: float = 0.5,
+                 vertical_box_clearance: float = 0.5,
+                 max_box_height: float = 4,
+                 face_color=Config.DRAWER_BOX_COLOR,
+                 **kwargs):
+        self.opening_width = opening_width
+        self.opening_height = opening_height
+        self.length = length
+        self.face_material = face_material
+        self.reveal = reveal
+        self.drawer_slide_thickness = drawer_slide_thickness
+        self.vertical_box_clearance = vertical_box_clearance
+        super().__init__(**kwargs)
+        if isinstance(reveal, float):
+            reveal = (reveal,)*4
+        elif isinstance(reveal, tuple) and len(reveal) == 1:
+            reveal = reveal*4
+        else:
+            # TODO: Assert len(reveal) == 4
+            # Reveal is (left, bottom, right, top)
+            ...
+
+        face_width = opening_width - (reveal[0] + reveal[2])
+        face_height = opening_height - (reveal[1] + reveal[3])
+        
+        self.face = RectangularComponent(
+            name='inset drawer face',
+            width=face_width,
+            height=face_height,
+            material=self.face_material,
+            color=face_color,
+            position=Position(
+                x=reveal[0],
+                y=0,
+                z=reveal[1],
+            ),
+            orientation=Orientation(
+                rx=0,
+                ry=0,
+                rz=0,
+            ),
+        )
+        self.add_child(self.face)
+        box_width = opening_width - 2*drawer_slide_thickness
+        box_height = min(opening_height - 2*vertical_box_clearance, max_box_height)
+        self.box = Qtr3DrawerBox(
+            width=box_width,
+            height=box_height,
+            length=length,
+            box_material=box_material,
+            bottom_material=bottom_material,
+            position=Position(
+                x=drawer_slide_thickness,
+                y=face_material.thickness,
+                z=vertical_box_clearance,
+            ),
+        )
+        self.add_child(self.box)
+
+
 
 
 class ShakerDrawerFace(ShakerFramedPanel):
@@ -69,14 +231,14 @@ class BlumDrawer(ComponentContainer):
         super().__init__(*args, **kwargs)
         self.opening_width = opening_width
         self.opening_height = opening_height
-        
+
         if opening_width >= 24:
-            default_box_material = Material.PLY_3_4
-            default_bottom_material = Material.PLY_1_2
+            default_box_material = Material.PLY_3_4_4x8
+            default_bottom_material = Material.PLY_1_2_4x8
         else:
-            default_box_material = Material.PLY_1_2
-            default_bottom_material = Material.PLY_1_4
-            
+            default_box_material = Material.PLY_1_2_4x8
+            default_bottom_material = Material.PLY_1_4_LITERAL_4x8
+
         self.box_material = box_material if box_material is not None else default_box_material
         self.bottom_material = bottom_material if bottom_material is not None else default_bottom_material
         self.box_height = box_height if box_height is not None else (
